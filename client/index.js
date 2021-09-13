017f97bbca012ed597fbc080df74874ea1e746d5
Lea Casarin <leandro-c@users.noreply.github.com>
2021-09-13 10:33:28 -0300

    fixed the issue related to the deprecated body-parser, update the functionaluty with express

--- client/index.js
+++ client/index.js
@@ -1,14 +1,23 @@
 const express = require('express')
-const usersController = require('./usersController')
-const assetsController = require('./assetsController')
+const usersController = require('./UserController')
+const assetsController = require('./AssetController')
 
 // app initialization
 const app = express()
 
 // need to have this for express to parse json...
 const bodyParser = require("body-parser");
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded())
+
+/* app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(bodyParser.json()); */
+	
+// Parsers for POST data
+app.use(express.urlencoded({ extended: true, limit: '20mb' }));
+app.use(express.json({limit: '20mb'}));
+
 
 // adds the CRUD services
 usersController(app)
