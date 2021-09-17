56822763c1ede6b981914f53cba5e597b83ea7bf
Lea Casarin <leandro-c@users.noreply.github.com>
2021-09-16 23:57:20 -0300

    build data estructura for pieChart

--- src/components/CakeChart.js
+++ src/components/CakeChart.js
@@ -1,6 +1,21 @@
-import React from 'react';
+import React, { useEffect }  from 'react';
 import {Chart} from 'react-google-charts'
+import { useSelector, useDispatch } from 'react-redux';
+import { getUserReducer } from '../features/user/userSlice'
+
 const CakeChart = () => {
+
+    const dispatch = useDispatch();
+    const userStatus = useSelector(state => state.user.status)
+    const user = useSelector(state => state.user.user);
+    useEffect(() => {
+        if (userStatus === 'idle') {
+            dispatch(getUserReducer())
+        }
+    }, [])
+    let data =[]
+    data =user.username?Object.keys(user["assets"]).map((key) => [key, user["assets"][key]]):[];
+    console.log(data)
     return (
         <div>
             <Chart
